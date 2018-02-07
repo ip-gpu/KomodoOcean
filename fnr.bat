64835cd20139634a0cbb4e228ade6a7b4b0113ea
DeckerSU <support@decker.su>
2018-02-07 10:35:33 +0300

    changed fnr.bat to autodetect current project dir

--- fnr.bat
+++ fnr.bat
@@ -1,1 +1,4 @@
-fnr --cl --dir "D:\KomodoOcean" --fileMask "Makefile.Release" --useRegEx --find " -DNDEBUG" --replace ""
+@echo off
+set datapath=%~dp0
+IF %datapath:~-1%==\ SET datapath=%datapath:~0,-1%
+fnr --cl --dir "%datapath%" --fileMask "Makefile.Release" --useRegEx --find " -DNDEBUG" --replace ""
