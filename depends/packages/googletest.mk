c51b2b512d8523a2d0a56461e74931bd45ec3819
DeckerSU <deckersu@protonmail.com>
2021-02-07 11:18:06 +0300

    googletest: changes to cross-compile for darwin

--- depends/packages/googletest.mk
+++ depends/packages/googletest.mk
@@ -1,9 +1,14 @@
 package=googletest
-$(package)_version=1.7.0
+$(package)_version=1.8.1
 $(package)_download_path=https://github.com/google/$(package)/archive/
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
 $(package)_download_file=release-$($(package)_version).tar.gz
-$(package)_sha256_hash=f73a6546fdf9fce9ff93a5015e0333a8af3062a152a9ad6bcb772c96687016cc
+$(package)_sha256_hash=9bf1fe5182a604b4135edc1a425ae356c9ad15e9b23f9f12a02e80184c3a249c
+
+define $(package)_set_vars
+$(package)_cxxflags+=-std=c++11
+$(package)_cxxflags_linux=-fPIC
+endef
 
 ifeq ($(build_os),darwin)
 define $(package)_set_vars
@@ -14,16 +19,29 @@ endif
 ifeq ($(build_os),darwin)
 $(package)_install=ginstall
 define $(package)_build_cmds
-    $(MAKE) -C make gtest.a
+    $(MAKE) -C googlemock/make gmock.a && \
+    $(MAKE) -C googletest/make gtest.a
 endef
 else
 $(package)_install=install
+
+ifeq ($(build_os)$(host_os),linuxdarwin) # cross-compile for MacOS from Linux
 define $(package)_build_cmds
-  $(MAKE) -C make CXXFLAGS=-fPIC gtest.a
+  $(MAKE) -C googlemock/make CC="$(build_prefix)/bin/$($(package)_cc)" CXX="$(build_prefix)/bin/$($(package)_cxx)" AR="$(build_prefix)/bin/$($(package)_ar)" CXXFLAGS="$($(package)_cxxflags)" gmock.a && \
+  $(MAKE) -C googletest/make CC="$(build_prefix)/bin/$($(package)_cc)" CXX="$(build_prefix)/bin/$($(package)_cxx)" AR="$(build_prefix)/bin/$($(package)_ar)" CXXFLAGS="$($(package)_cxxflags)" gtest.a
 endef
+else
+define $(package)_build_cmds
+  $(MAKE) -C googlemock/make CC="$($(package)_cc)" CXX="$($(package)_cxx)" AR="$($(package)_ar)" CXXFLAGS="$($(package)_cxxflags)" gmock.a && \
+  $(MAKE) -C googletest/make CC="$($(package)_cc)" CXX="$($(package)_cxx)" AR="$($(package)_ar)" CXXFLAGS="$($(package)_cxxflags)" gtest.a
+endef
+endif
 endif
 
 define $(package)_stage_cmds
-  $($(package)_install) -D ./make/gtest.a $($(package)_staging_dir)$(host_prefix)/lib/libgtest.a && \
-  cp -a ./include $($(package)_staging_dir)$(host_prefix)/include
+  mkdir -p $($(package)_staging_dir)$(host_prefix)/lib && \
+  install ./googlemock/make/gmock.a $($(package)_staging_dir)$(host_prefix)/lib/libgmock.a && \
+  install ./googletest/make/gtest.a $($(package)_staging_dir)$(host_prefix)/lib/libgtest.a && \
+  cp -a ./googlemock/include $($(package)_staging_dir)$(host_prefix)/ && \
+  cp -a ./googletest/include $($(package)_staging_dir)$(host_prefix)/
 endef
