5963c58ffa2c017c7b7f780edb8524100ecde032
DeckerSU <deckersu@protonmail.com>
2021-02-26 01:42:43 +0300

    update build scripts for mac-cross, win and mac-native

--- zcutil/build-mac-cross.sh
+++ zcutil/build-mac-cross.sh
@@ -6,8 +6,6 @@ tar -C ${PWD}/depends/SDKs -xf ${PWD}/Xcode-11.3.1-11C505-extracted-SDK-with-lib
 # make deps
 make -C ${PWD}/depends v=1 NO_PROTON=1 HOST=x86_64-apple-darwin18 DARWIN_SDK_PATH=${PWD}/depends/SDKs/Xcode-11.3.1-11C505-extracted-SDK-with-libcxx-headers -j$(nproc --all)
 ./autogen.sh
-CPPFLAGS="-fvisibility=hidden -fvisibility-inlines-hidden" \
-CXXFLAGS="-fvisibility=hidden -fvisibility-inlines-hidden" \
 LDFLAGS="-Wl,-no_pie" \
 CONFIG_SITE="$PWD/depends/x86_64-apple-darwin18/share/config.site" ./configure --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
 # make app
--- zcutil/build-win.sh
+++ zcutil/build-win.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 HOST=x86_64-w64-mingw32
 CXX=x86_64-w64-mingw32-g++-posix
 CC=x86_64-w64-mingw32-gcc-posix
@@ -9,15 +10,22 @@ set -eu -o pipefail
 set -x
 cd "$(dirname "$(readlink -f "$0")")/.."
 
-cd depends/ && make HOST=$HOST V=1 -j$(nproc) && cd ../
+build_old () {
+    # unused for now (old approach)
+    cd depends/ && make HOST=$HOST V=1 -j$(nproc) && cd ../
+    ./autogen.sh
 
+    CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site \
+        CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread -g0 -O2" \
+        ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-shared \
+        --with-gui=qt5 --disable-bip70 --enable-tests=no
+
+    sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
+    cd src/
+    CC="${CC}" CXX="${CXX}" make "$@" V=1
+}
+
+make -C ${PWD}/depends V=1 HOST=x86_64-w64-mingw32 -j$(nproc --all)
 ./autogen.sh
-
-CONFIG_SITE=$PWD/depends/x86_64-w64-mingw32/share/config.site \
-    CXXFLAGS="-DPTW32_STATIC_LIB -DCURL_STATICLIB -DCURVE_ALT_BN128 -fopenmp -pthread -g0 -O2" \
-    ./configure --prefix="${PREFIX}" --host=x86_64-w64-mingw32 --enable-static --disable-shared \
-    --with-gui=qt5 --disable-bip70 --enable-tests=no
-
-sed -i 's/-lboost_system-mt /-lboost_system-mt-s /' configure
-cd src/
-CC="${CC}" CXX="${CXX}" make "$@" V=1
+CONFIG_SITE="$PWD/depends/x86_64-w64-mingw32/share/config.site" CXXFLAGS="-DCURL_STATICLIB" ./configure --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
+make V=1 -j$(nproc --all)
