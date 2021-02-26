0e64db9c02840360a5107028664dff56c7b39b97
DeckerSU <deckersu@protonmail.com>
2021-02-26 09:03:25 +0300

    fix build-mac.sh (darwin native build)

--- zcutil/build-mac.sh
+++ zcutil/build-mac.sh
@@ -5,5 +5,5 @@ sed -i.old -e 's|\(CURVE=ALT_BN128[ \t]*MULTICORE=\)\([0-9]\{1,\}\)|\10|' ./depe
 make -C ${PWD}/depends v=1 NO_PROTON=1 HOST=x86_64-apple-darwin18 -j$(nproc --all)
 ./autogen.sh
 CXXFLAGS="-g0 -O2" \
-CONFIG_SITE="$PWD/depends/x86_64-apple-darwin18/share/config.site" ./configure --disable-tests --disable-bench ---with-gui=qt5 --disable-bip70
+CONFIG_SITE="$PWD/depends/x86_64-apple-darwin18/share/config.site" ./configure --disable-tests --disable-bench --with-gui=qt5 --disable-bip70
 make V=1 -j$(nproc --all)
