6d2fb961a8676775577d23fac46e0b2e877717c0
DeckerSU <deckersu@protonmail.com>
2021-11-15 14:59:09 +0300

    depends: Qt 5.12.11
    - switch to Qt 5.12.11 (follow Bitcoin upstream)
    - switch from system zlib to qt zlib
    - add libxkbcommon (since we don't use xkbcommon library bundled with Qt)
    
    TODO: check correctness of QMAKE_CC, QMAKE_CXX vars in qtbase/mkspecs/common/clang.conf
    during compile and cross-compile for Darwin.

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -7,9 +7,9 @@ else
 endif
 
 qt_native_packages = native_protobuf
-qt_packages = qrencode protobuf zlib
+qt_packages = qrencode protobuf
 
-qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig
+qt_linux_packages:=qt expat libxcb xcb_proto libXau xproto freetype fontconfig libxkbcommon
 qt_android_packages=qt
 
 qt_darwin_packages=qt
